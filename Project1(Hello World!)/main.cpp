abe7cfdaa7d153ac5e7f068ec8fa4f4c98312ae1
Bekhruz Tukhtasinov <221430@akfauniversity.org>
2022-09-14 14:44:22 +0500

    Initial Commit

---
+++ Project1(Hello World!)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project1(Hello World!)
+//
+//  Created by Bekhruz Tukhtasinov on 14/09/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\nMy name is Bekhruz and this is my 1st project :)\n";
+    return 0;
+}
